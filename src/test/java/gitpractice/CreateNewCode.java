e3f7f4c12382625bf81fb723354e05cd4d7330b7
Shivunayak <Shivunayak@Shivu>
2023-01-19 21:15:27 +0530

    new code

---
+++ src/test/java/gitpractice/CreateNewCode.java
@@ -0,0 +1,13 @@
+package gitpractice;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+public class CreateNewCode {
+	
+	@Test
+	public void codePushed() {
+		
+		System.out.println("code pushed successfully");
+	}
+
+}
